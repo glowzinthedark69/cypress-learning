df3baf4374e9c8f62445902ece0ce41e47dff3a0
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-06-22 16:34:58 -0600

    Another assertion

---
+++ cypress/e2e/Sample4.cy.js
@@ -0,0 +1,24 @@
+describe('Test Suite for API Project Swagger Doc', () => {
+    beforeEach(() => {
+      // Visit the homepage before each test
+      cy.visit('http://127.0.0.1:8000/docs')
+    })
+  
+    it('Should navigate to the homepage successfully', () => {
+      // Assert that the current URL is the homepage
+      cy.url().should('include', '/docs')
+    })
+  
+    it('Find the summary for the first GET listed for /locations', () => {
+      // Assert that the navbar exists on the homepage
+      cy.get('.opblock-summary-description').contains('Get Locations')
+    })
+    
+    it('Find the summary for the first POST listed for /locations', () => {
+        // Assert that the navbar exists on the homepage
+        //cy.get('.opblock-summary-description').contains('Create Location'),
+        cy.get('.opblock-summary-description').contains('Create Location').click(),
+        cy.get('.opblock-summary-description').contains('Get Location').click()
+      })
+    // More tests...
+  })
