dea087aca75d3b7e4eead8bd29d1f2d60d4cfc97
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-06-25 09:30:56 -0600

    CL-1: Initial file creation and first smart commit

---
+++ cypress/e2e/Exceptions_Test.cy.js
@@ -0,0 +1,6 @@
+describe('Test Suite for Login Test Page', () => {
+    beforeEach(() => {
+        // Visit the homepage before each test
+        cy.visit('https://practicetestautomation.com/practice-test-exceptions/')
+    })
+})
