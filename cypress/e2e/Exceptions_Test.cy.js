48794000b18c1a55a7bd0a342a70a2b50726f525
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-06-26 13:10:16 -0600

    First few tests created

--- cypress/e2e/Exceptions_Test.cy.js
+++ cypress/e2e/Exceptions_Test.cy.js
@@ -1,6 +1,22 @@
-describe('Test Suite for Login Test Page', () => {
+describe('Test Suite for Test Exceptions Page', () => {
     beforeEach(() => {
         // Visit the homepage before each test
         cy.visit('https://practicetestautomation.com/practice-test-exceptions/')
+
+        // Listen for uncaught exceptions
+        cy.on('uncaught:exception', (err, runnable) => {
+            // when the exception happens, we check if it's the error we are expecting
+            expect(err.message).to.include('Assignment to constant variable.')
+            // returning false here prevents Cypress from failing the test
+            return false
     })
+})
+
+    it('Validate NoSuchElementException', () => {
+        // Click on the Add button
+        cy.get('[id=add_btn]').click()
+        
+        // Wait for the element to load and assert it contains an input-field class
+        cy.get('#row2', { timeout: 10000 }).should('exist')
+    }) 
 })
