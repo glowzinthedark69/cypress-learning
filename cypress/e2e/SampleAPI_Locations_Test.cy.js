5cf7b99531a370b986c0f0e5d0ab29c8ac8f7c34
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-07-20 13:56:29 -0600

    Updating tests; adding some for /Users endpoint

--- cypress/e2e/SampleAPI_Locations_Test.cy.js
+++ cypress/e2e/SampleAPI_Locations_Test.cy.js
@@ -11,22 +11,22 @@ describe("API Test", () => {
         return Cypress._.every(array, Cypress._.isObject);
       });
 
-      // // Assert that the correct properties exist for each object in the array
-      // expect(response.body.length).to.be.above(0);
-      // response.body.forEach((item) => {
-      //   expect(item).to.be.an("object");
-      //   expect(item).to.have.property("id");
-      //   expect(item.id).to.not.be.null;
-      //   expect(typeof item.id).to.eql("number");
-      //   expect(item.id).to.be.above(-1);
-      //   expect(item.id).to.not.equal(0);
-      //   expect(item).to.have.property("city");
-      //   expect(item.city).to.not.be.null;
-      //   expect(item.city).to.be.a("string");
-      //   expect(item).to.have.property("country");
-      //   expect(item.country).to.not.be.null;
-      //   expect(item.country).to.be.a("string");
-      // });
+      // // Assert that the correct properties exist for a determined number of random objects in the array
+      const sample = Cypress._.sampleSize(response.body, 250); // Adjust sample size as needed
+      sample.forEach((item) => {
+        expect(item).to.be.an("object");
+        expect(item).to.have.property("id");
+        expect(item.id).to.not.be.null;
+        expect(typeof item.id).to.eql("number");
+        expect(item.id).to.be.above(-1);
+        expect(item.id).to.not.equal(0);
+        expect(item).to.have.property("city");
+        expect(item.city).to.not.be.null;
+        expect(item.city).to.be.a("string");
+        expect(item).to.have.property("country");
+        expect(item.country).to.not.be.null;
+        expect(item.country).to.be.a("string");
+      });
 
       // Assert the properties of the first object in the response array
       expect(response.body[0]).to.have.property("id", 96145);
---
+++ cypress/e2e/SampleAPI_Users_Test.cy.js
@@ -0,0 +1,48 @@
+describe("API Test", () => {
+  it("Validate JSON response", () => {
+    // Make a GET request to your API endpoint
+    cy.request("GET", "http://127.0.0.1:8000/users").then((response) => {
+      // Assert that the response status is 200
+      expect(response.status).to.eq(200);
+      // Assert that the response body is an array
+      expect(response.body).to.be.an("array");
+      // Assert that every item in the array is an object
+      expect(response.body).to.satisfy((array) => {
+        return Cypress._.every(array, Cypress._.isObject);
+      });
+
+      // // Assert that the correct properties exist for a determined number of random objects in the array
+      const sample = Cypress._.sampleSize(response.body, 250); // Adjust sample size as needed
+      sample.forEach((item) => {
+        expect(item).to.be.an("object");
+        expect(item).to.have.property("userId");
+        expect(item.id).to.not.be.null;
+        expect(typeof item.userId).to.eql("number");
+        expect(item.userId).to.be.above(-1);
+        expect(item.userId).to.not.equal(0);
+        expect(item).to.have.property("name");
+        expect(item.name).to.not.be.null;
+        expect(item.name).to.be.a("string");
+        expect(item).to.have.property("city");
+        expect(item.city).to.not.be.null;
+        expect(item.city).to.be.a("string");
+        expect(item).to.have.property("country");
+        expect(item.country).to.not.be.null;
+        expect(item.country).to.be.a("string");
+        expect(item).to.have.property("jobTitle");
+        expect(item.jobTitle).to.not.be.null;
+        expect(item.jobTitle).to.be.a("string");
+      });
+
+      // Assert the properties of the first object in the response array
+      expect(response.body[0]).to.have.property("userId", 40526);
+      expect(response.body[0]).to.have.property("name", "Albert Macejkovic");
+      expect(response.body[0]).to.have.property("city", "Hickleberg");
+      expect(response.body[0]).to.have.property("country", "Yemen");
+      expect(response.body[0]).to.have.property(
+        "jobTitle",
+        "Internal Creative Liaison"
+      );
+    });
+  });
+});
